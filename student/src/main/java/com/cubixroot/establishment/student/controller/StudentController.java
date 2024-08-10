af8371ede06ee9424891fdb00a055056e05acc5b
Lionel Traore <hello@lioneltraore.com>
2024-08-10 03:33:20 +0000

    add student controller

---
+++ student/src/main/java/com/cubixroot/establishment/student/controller/StudentController.java
@@ -0,0 +1,31 @@
+package com.cubixroot.establishment.student.controller;
+
+import com.cubixroot.establishment.student.domain.Student;
+import com.cubixroot.establishment.student.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService studentService;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveStudent(@RequestBody Student student) {
+        studentService.saveStudent(student);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents() {
+        return ResponseEntity.ok(studentService.findAllStudents());
+    }
+
+}
