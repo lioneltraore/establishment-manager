502ff0025abf0d72a1c8c551191590246bbf3ff5
Lionel Traore <hello@lioneltraore.com>
2024-08-10 02:36:20 +0000

    add student service

---
+++ student/src/main/java/com/cubixroot/establishment/student/service/StudentService.java
@@ -0,0 +1,24 @@
+package com.cubixroot.establishment.student.service;
+
+import com.cubixroot.establishment.student.domain.Student;
+import com.cubixroot.establishment.student.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public void saveStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    public List<Student> findAllStudents() {
+        return studentRepository.findAll();
+    }
+
+}
