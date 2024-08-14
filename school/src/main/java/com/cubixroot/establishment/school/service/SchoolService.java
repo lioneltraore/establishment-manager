28be37e04992af52159bb3dba9e6c17fb8356c41
Lionel Traore <hello@lioneltraore.com>
2024-08-14 23:11:42 +0000

    add communication between microservices

--- school/src/main/java/com/cubixroot/establishment/school/service/SchoolService.java
+++ school/src/main/java/com/cubixroot/establishment/school/service/SchoolService.java
@@ -1,10 +1,13 @@
 package com.cubixroot.establishment.school.service;
 
 
+import com.cubixroot.establishment.school.config.client.StudentClient;
 import com.cubixroot.establishment.school.domain.School;
 import com.cubixroot.establishment.school.domain.dto.FullSchoolDTO;
+import com.cubixroot.establishment.school.domain.dto.StudentDTO;
 import com.cubixroot.establishment.school.repository.SchoolRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +17,9 @@ import java.util.List;
 public class SchoolService {
 
     private final SchoolRepository schoolRepository;
+
+    @Autowired
+    private StudentClient studentClient;
 
     public void saveSchool(School school) {
         schoolRepository.save(school);
@@ -27,9 +33,15 @@ public class SchoolService {
         School school = schoolRepository.findById(schoolId)
                 .orElse(School.builder()
                         .name("NOT_FOUND")
-                        .name("NOT_FOUND")
+                        .email("NOT_FOUND")
                         .build());
-        return null;
-    }
 
+        List<StudentDTO> students = studentClient.findAllStudentsBySchool(schoolId);
+
+        return FullSchoolDTO.builder()
+                .name(school.getName())
+                .email(school.getEmail())
+                .students(students)
+                .build();
+    }
 }
